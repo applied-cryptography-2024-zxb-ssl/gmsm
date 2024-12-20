24a0a6c55c480a1059fe5c10f10f82a26449101a
Baiting Jiang <baitingjiang@buaa.edu.cn>
2024-12-20 13:34:10 +0800

    [update] add sm3 context

--- own_sm3/main/main.go
+++ own_sm3/main/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	message := []byte("abc")
-	hash := sm3.SM3Hash(message)
+	hash := sm3.Sum(message)
 	s := fmt.Sprintf("%x", hash)
 
 	if s != "66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0" {
@@ -17,7 +17,7 @@ func main() {
 	}
 
 	message = []byte("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
-	hash = sm3.SM3Hash(message)
+	hash = sm3.Sum(message)
 	s = fmt.Sprintf("%x", hash)
 	if s != "debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732" {
 		panic("PASS Test_2")
@@ -26,7 +26,7 @@ func main() {
 	}
 
 	message = []byte("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc")
-	hash = sm3.SM3Hash(message)
+	hash = sm3.Sum(message)
 	s = fmt.Sprintf("%x", hash)
 	if s != "952eb84cacee9c10bde4d6882d29d63140ba72af6fe485085095dccd5b872453" {
 		panic("PASS Test_3")
@@ -35,7 +35,7 @@ func main() {
 	}
 
 	message = []byte("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
-	hash = sm3.SM3Hash(message)
+	hash = sm3.Sum(message)
 	s = fmt.Sprintf("%x", hash)
 	if s != "90d52a2e85631a8d6035262626941fa11b85ce570cec1e3e991e2dd7ed258148" {
 		panic("PASS Test_4")
@@ -44,7 +44,7 @@ func main() {
 	}
 
 	message = []byte("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc")
-	hash = sm3.SM3Hash(message)
+	hash = sm3.Sum(message)
 	s = fmt.Sprintf("%x", hash)
 	if s != "e1c53f367a9c5d19ab6ddd30248a7dafcc607e74e6bcfa52b00e0ba35e470421" {
 		panic("PASS Test_5")
@@ -53,7 +53,7 @@ func main() {
 	}
 
 	message = []byte("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc")
-	hash = sm3.SM3Hash(message)
+	hash = sm3.Sum(message)
 	s = fmt.Sprintf("%x", hash)
 	if s != "520472cafdaf21d994c5849492ba802459472b5206503389fc81ff73adbec1b4" {
 		panic("PASS Test_6")
